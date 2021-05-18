299ae519256d767abdb0abb1f754ae3ee3f06aa2
Witold Kaczkowski <78959807+Widzisz@users.noreply.github.com>
2021-05-18 14:17:56 +0200

    Add first look of WelcomePage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { creat_event } from './components/creat_event/creat_event';
-import { creat_user } from './components/creat_user/creat_user';
-import { main_page } from './components/main_page/main_page';
-import { user_profile } from './components/user_profile/user_profile';
-import { welcome_page } from './components/welcome_page/welcome_page';
-
 import './App.scss';
+import WelcomePage from './components/welcome_page/WelcomePage';
+import CreatEvent from './components/creat_event/CreatEvent';
+import { creat_user } from './components/creat_user/CreatUser';
+import { main_page } from './components/main_page/MainPage';
+import { user_profile } from './components/user_profile/UserProfile';
 
 const App = () => {
     return (
         <div className="App">
-            <h1>Game Board Events</h1>
+            <WelcomePage />
         </div>
     );
 };
