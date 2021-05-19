0b55f23e943d9e47e92fbcdbd5fb5b1da1557bd9
Witold Kaczkowski <78959807+Widzisz@users.noreply.github.com>
2021-05-20 00:40:36 +0200

    Form userCreat is working

--- src/components/creat_user/CreatUser.js
+++ src/components/creat_user/CreatUser.js
@@ -1,33 +1,62 @@
 import React from 'react';
 import './creat_user.scss';
-import useForm from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+//connection between ReactHook and Yup
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+    userName: yup.string().required('How should I call you?'),
+    email: yup.string().email().required(),
+    password: yup.string().min(4).max(10).required(),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null]),
+});
 
 const CreatUser = () => {
-    const { register, handleSubmit, errors } = useForm();
-
-    const onSubmit = data => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+    const submitForm = data => {
         console.log(data);
-        // w przyszłosci trzeba podpiąc FIREBASE
     };
-
     return (
         <section className="form__container">
-            <h2>Join us ! Never travel the world alone again</h2>
+            <h2 className="form__title">Join us!</h2>
 
-            <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-                <label>Username:</label>
-                <input type="text" name="username" ref={register} />
-
-                <label>Email:</label>
-                <input type="email" name="email" ref={register} />
-
-                <label>Password:</label>
-                <input type="password" name="password" ref={register} />
-
-                <label>Repeat Password:</label>
-                <input type="password" name="password2" ref={register} />
-
-                <button type="submit">Submit</button>
+            <form className="form" onSubmit={handleSubmit(submitForm)}>
+                <input
+                    type="text"
+                    name="firstName"
+                    placeholder="Your Name..."
+                    {...register('userName')}
+                />
+                <p>{errors.userName?.message}</p>
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email..."
+                    {...register('email')}
+                />
+                <p>{errors.email?.message}</p>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password..."
+                    {...register('password')}
+                />
+                <p>{errors.password?.message}</p>
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm Password..."
+                    {...register('confirmPassword')}
+                />
+                <p>{errors.confirmPassword && 'Password Should Match!'}</p>
+                <input type="submit" id="submit" />
             </form>
         </section>
     );
